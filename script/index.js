a2407b03ec3b8022f84987bd1f8390e23f022690
moacyr-leite <moacyricardofl2@hotmail.com>
2024-01-14 16:20:54 -0300

    1st commit

---
+++ script/index.js
@@ -0,0 +1,8 @@
+const navegacao = document.querySelector('.cabecalho__navegacao');
+
+navegacao.innerHTML = `
+    <a href="index.html" class="cabecalho__navegacao__item">Home</a>
+    <a href="html/about.html" class="cabecalho__navegacao__item">Sobre Mim</a>
+    <a href="html/curiculum.html" class="cabecalho__navegacao__item">Currículo</a>
+    <a href="html/myproject.html" class="cabecalho__navegacao__item"></a>
+`
---
+++ script/modal-page.js
@@ -0,0 +1,8 @@
+const navegacao = document.querySelector('.cabecalho__navegacao');
+
+navegacao.innerHTML = `
+    <a href="../index.html" class="cabecalho__navegacao__item">Home</a>
+    <a href="about.html" class="cabecalho__navegacao__item">Sobre Mim</a>
+    <a href="curiculum.html" class="cabecalho__navegacao__item">Currículo</a>
+    <a href="myproject.html" class="cabecalho__navegacao__item"></a>
+`
